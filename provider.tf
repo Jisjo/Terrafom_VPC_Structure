827812236836ceed9e9ff7bda2c2235e5f5079e2
Jisjo <61861646+Jisjo@users.noreply.github.com>
2021-12-03 21:08:59 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.region
-  access_key = "AKIAVOYUBUYWPOTHCWH6"
-  secret_key = "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"
+  access_key = "xxxxxxxxxxxxxxxxxxxx"
+  secret_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 }
